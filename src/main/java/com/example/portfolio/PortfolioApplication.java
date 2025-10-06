ecb5b3b8bab0bcea0b2153e3c21215167766c47f
Kota_Mukai <kota1022@mukai@gmail.com>
2025-10-07 00:40:40 +0900

    pull request #22

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -2,12 +2,17 @@ package com.example.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    Neo4jDataAutoConfiguration.class,
+    Neo4jAutoConfiguration.class
+})
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
 
 }
